2a5f48188876818a4e4dd69969bf2ca3f90cb1fb
Jason Walsh <jwalsh@saylent.com>
2017-06-12 17:35:44 -0400

    mod-d depends on v2 of mod-a

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 import * as a from '@jwalsh/mod-a'
-// import * as b from '@jwalsh/mod-b'
-// import * as c from '@jwalsh/mod-c'
-// import * as d from '@jwalsh/mod-d'
+import * as b from '@jwalsh/mod-b'
+import * as c from '@jwalsh/mod-c'
+import * as d from '@jwalsh/mod-d'
 
 console.log('Test harness for module dependency changes between NPM 3 and NPM 5')
 
-console.log(a, a._(1))
-// console.log(b)
-// console.log(c)
-// console.log(d)
+console.log(a._(1))
+console.log(b._(2))
+console.log(c._(3))
+console.log(d._(4))
